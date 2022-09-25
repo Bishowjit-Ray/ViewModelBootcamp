cb46ddf144dd1a0c5df198650d6a9b4351dd13a7
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-09-25 11:03:01 +0600

    Add files via upload

---
+++ ViewModelBootCamp/ViewModelBootCamp/ViewModelBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelBootCampApp.swift
+//  ViewModelBootCamp
+//
+//  Created by Bishowjit Ray on 23/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewModelBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
